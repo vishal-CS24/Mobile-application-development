762737cfe017519793996dddff0593be916d069c
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-06-10 23:59:10 +0530

    Add files via upload

--- this.js
+++ this.js
@@ -12,3 +12,8 @@ const friendGreetings={
 friendGreetings.greet=greetings.greet
 //it prints greetings with new name
 friendGreetings.greet()
+
+const greet = greetings.greet.bind({name:"hello this is vishal gupta calling bind function"})
+greet()
+greetings.greet.call({name:"this is call function and bound object"})
+greetings.greet.apply({name:"this is apply function and bound     object"})
